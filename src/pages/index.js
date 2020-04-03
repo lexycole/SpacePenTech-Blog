59feb98758fbaff1df39982afee8be351f96b4eb
olawale akinro <40228505+lexycole@users.noreply.github.com>
2020-04-03 19:28:49 +0100

    feat: I built a thing

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Layout from '../components/layout';
+import PostPreview from '../components/post-preview';
+import Insta from '../components/insta';
+import usePosts from '../hooks/use-posts';
+import Hero from '../components/hero';
+
+export default () => {
+    const posts = usePosts();
+    
+    return (
+        <>
+        <Hero />
+    <Layout>
+   <h2>Read my blogs</h2>
+   {posts.map(post => (
+      <PostPreview key={post.slug} post={post}/>
+   ))}
+   <Insta />
+    </Layout>
+   </>
+    );
+};
